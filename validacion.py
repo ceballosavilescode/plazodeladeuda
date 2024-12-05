6f0dae6e744a9defd23f7630edcb73dcf77bd07c
ceballosavilescode <ceballosavilescode@gmail.com>
2024-12-05 16:04:53 -0300

    Update validacion.py

--- validacion.py
+++ validacion.py
@@ -1,11 +1,11 @@
 print(f"el usuario se registro como 02 : {{ usuario }}")
 
 # Variable usuario
-usuario = {{ usuario }}
+usuario1 = {{ usuario }}
 
-print(f"El usuario se registró como: ", usuario)
+print(f"El usuario se registró como: ", usuario1)
 
-if usuario == 'ceballos':
+if usuario1 == 'ceballos':
 	devengo = pd.DataFrame()
 	for f in glob.glob("../interfaz/input_excel/pagoManual/*"): # "../mejorninez/input_excel/pagoManual/*",
 		df = pd.read_excel(f, engine='openpyxl', converters={'folio': str, 'Nº CDP': str, 'Monto Total': int})
