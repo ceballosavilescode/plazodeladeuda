6bead59590abbb1f1b240dde749e6832d54fd243
ceballosavilescode <ceballosavilescode@gmail.com>
2024-12-05 16:28:33 -0300

    Update validacion.py

--- validacion.py
+++ validacion.py
@@ -1,7 +1,7 @@
 print(f"El usuario se registró como 08: {usuario}")
 
 # Variable usuario
-usuario1 = usuario
+usuario1 = {usuario}
 
 # Imprimir el nombre del usuario
 print(f"El usuario se registró como: {usuario1}")
