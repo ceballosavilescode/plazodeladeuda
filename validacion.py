61a0b0f752aed160acf597232c5324508e5bb13b
ceballosavilescode <ceballosavilescode@gmail.com>
2024-12-05 23:13:20 -0300

    Update validacion.py

--- validacion.py
+++ validacion.py
@@ -4,7 +4,7 @@ import glob
 import sqlalchemy
 
 # Mostrar mensaje de registro con el usuario
-print(f"El usuario se registró como 1.8: {usuario}")
+print(f"El usuario se registró como 1.9: {usuario}")
 
 # Asignar la variable usuario a usuario1
 usuario1 = usuario
